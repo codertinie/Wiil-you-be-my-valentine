adfd367b1a399a83d105d23d2fb3803c16044ac6
codertinie <munyao25@gmail.com>
2025-02-13 03:50:19 +0300

    added some music

--- script.js
+++ script.js
@@ -5,29 +5,36 @@ function showLove() {
             document.querySelector('.buttons').style.display = 'none';
             document.body.style.backgroundColor = '#ff99b4';
             confettiEffect();
-        }
+            playMusic()
+}
 
-        function moveButton() {
-            let button = document.querySelector('.no');
-            let maxX = window.innerWidth - button.clientWidth - 50;
-            let maxY = window.innerHeight - button.clientHeight - 50;
-            let x = Math.random() * maxX;
-            let y = Math.random() * maxY;
-            button.style.position = 'absolute';
-            button.style.left = `${x}px`;
-            button.style.top = `${y}px`;
-        }
+function moveButton() {
+        let button = document.querySelector('.no');
+        let maxX = window.innerWidth - button.clientWidth - 50;
+        let maxY = window.innerHeight - button.clientHeight - 50;
+        let x = Math.random() * maxX;
+        let y = Math.random() * maxY;
+        button.style.position = 'absolute';
+        button.style.left = `${x}px`;
+        button.style.top = `${y}px`;
+}
 
-        function confettiEffect() {
-            for (let i = 0; i < 30; i++) {
-                let confetti = document.createElement('div');
-                confetti.innerHTML = "❤️";
-                confetti.style.position = 'fixed';
-                confetti.style.left = Math.random() * 100 + 'vw';
-                confetti.style.top = '-10px';
-                confetti.style.fontSize = '2rem';
-                confetti.style.animation = `fall 2s linear ${Math.random()}s forwards`;
-                document.body.appendChild(confetti);
-                setTimeout(() => confetti.remove(), 2500);
-            }
-        }
+function confettiEffect() {
+    for (let i = 0; i < 30; i++) {
+        let confetti = document.createElement('div');
+        confetti.innerHTML = "❤️";
+        confetti.style.position = 'fixed';
+        confetti.style.left = Math.random() * 100 + 'vw';
+        confetti.style.top = '-10px';
+        confetti.style.fontSize = '2rem';
+        confetti.style.animation = `fall 5s linear ${Math.random()}s forwards`;
+        document.body.appendChild(confetti);
+        setTimeout(() => confetti.remove(), 5000);
+    }
+}
+function playMusic() {
+    let music = document.getElementById("backgroundMusic");
+    music.volume = 0.1;
+    music.play();
+}
+
